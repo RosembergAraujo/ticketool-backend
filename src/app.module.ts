7fa58c970898703bdf8674335c5ec3e1ba2cf4f1
Rosemberg Araujo <70284263+RosembergAraujo@users.noreply.github.com>
2023-02-06 13:27:08 -0300

    Creating auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { GeneralUserModule } from './general_user/general_user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, GeneralUserModule],
+  imports: [PrismaModule, GeneralUserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
