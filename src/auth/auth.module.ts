40d4703ddbe7463d14648a5521810bdee27d6544
Rosemberg Araujo <70284263+RosembergAraujo@users.noreply.github.com>
2023-02-13 00:04:51 -0300

    Returning Jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { GeneralUserModule } from '../general_user/general_user.module';
+import { JwtModule } from '@nestjs/jwt';
+import * as process from 'process';
+import { AppConstraints } from '../Constraints/AppConstraints';
 
 @Module({
-  imports: [GeneralUserModule],
+  imports: [
+    GeneralUserModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: AppConstraints.GENERAL_USER_TOKEN_TIME_TO_EXPIRE,
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,24 +3,38 @@ import { GeneralUserService } from '../general_user/general_user.service';
 import * as bcrypt from 'bcrypt';
 import { UnauthorizedError } from './errors/unauthorized.error';
 import { GeneralUser } from '../general_user/entities/general_user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: GeneralUserService) {}
+  constructor(
+    private readonly userService: GeneralUserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  login() {
-    throw new Error('Method not implemented.');
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   async validateUser(email: string, password: string): Promise<GeneralUser> {
     const user = await this.userService.findByEmail(email);
     if (user && (await bcrypt.compare(password, user?.password))) {
       return {
         ...user,
         password: undefined,
+        // foo: 'bar', // I can modify the return of "user" after Guards validates here
       };
     }
     throw new UnauthorizedError('Email address or password is incorrect');
+  }
+
+  login(user: GeneralUser): UserToken {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+    const jwtToken = this.jwtService.sign(payload);
+    return {
+      access_token: jwtToken,
+    };
   }
 }
